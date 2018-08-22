7194e79a86af96d3e793ba5812aea48be0b9adcc
nishant <nishant.singh87@gmail.com>
2018-08-22 12:41:35 +0530

    Add hot tickets to react app.

--- react-app/index.js
+++ react-app/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 
 import NavBar from "./components/nav-bar";
 import IntroCarousel from "./homepage/intro-carousel";
+import HotTickets from "./homepage/hot-tickets";
+// import BrowseByCategory from "./homepage/browse-by-category";
 
 const App = () =>(
     <React.Fragment>
       <NavBar/>
       <IntroCarousel/>
+      <HotTickets/>
     </React.Fragment>
 );
 
